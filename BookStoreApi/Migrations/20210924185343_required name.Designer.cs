931ec3e7dda5a1f1bd09383761633415173d4b2b
vldmrklnk <smile.traser@gmail.com>
2021-09-24 21:55:10 +0300

    upd db

--- BookStoreApi/Migrations/20210924185343_required name.Designer.cs
+++ BookStoreApi/Migrations/20210924185343_required name.Designer.cs
@@ -3,14 +3,16 @@ using BookStoreApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookStoreApi.Migrations
 {
     [DbContext(typeof(ApplicationDbcontext))]
-    partial class ApplicationDbcontextModelSnapshot : ModelSnapshot
+    [Migration("20210924185343_required name")]
+    partial class requiredname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
